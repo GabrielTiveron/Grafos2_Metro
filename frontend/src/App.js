655e0dde8d634ff75bfdfc6611621d56e24a6c62
Victor Peixoto <victorlevipeixoto@gmail.com>
2020-09-24 16:51:36 -0300

    Add graph library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
+import Graph from "react-graph-vis";
 import './App.css';
 
 function App() {
+  const graph = {
+		nodes: [
+			{'id': 'teste', 'label': 'teste', 'title': 'teste'},
+			{'id': 'teste2', 'label': 'teste2', 'title': 'teste2'},
+		],
+		edges:[
+			{'from': 'teste', 'to': 'teste2', 'label': '120'}
+		]
+  };
+ 
+  const options = {
+    layout: {
+    },
+    edges: {
+      color: "#000000"
+    },
+    height: "700px",
+  };
+ 
+  const events = {
+    select: function(event) {
+      var { nodes, edges } = event;
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			<Graph
+				style={{marginBottom: '0px', display: 'flex', flex: 1}}
+				graph={graph}
+				options={options}
+				events={events}
+			/>
   );
 }
 
 export default App;
+
